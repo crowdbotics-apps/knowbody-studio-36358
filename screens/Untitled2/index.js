350f1898788dc7165e5f959184a0b6d13fae5efa
Rodrigo Gadea <rodrigo@crowdbotics.com>
2022-07-25 14:09:54 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,4 +1,3 @@
-import { TextInput } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -9,30 +8,7 @@ const Untitled2 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      <View style={{
-      left: 33,
-      top: 79,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View><View style={{
-      left: 43,
-      top: 58,
-      position: "absolute",
-      width: 80,
-      height: 80,
-      backgroundColor: "#E4E4E4",
-      borderRadius: "50%"
-    }}></View><TextInput style={{
-      position: "absolute",
-      backgroundColor: "#ffffff",
-      borderColor: "#cccccc",
-      width: 150,
-      height: 30
-    }}></TextInput></View>;
+      </View>;
 };
 
 export default Untitled2;
